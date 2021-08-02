6d40826e2fb5bdc380ee6f6cc3d11e2544cd6712
black-code1 <legrandmahamat1@gmail.com>
2021-08-02 14:20:21 +0100

    MongoDB Mongoose

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
 
+mongoose.connect('mongodb+srv://legrand:legrand@cluster0.jgecv.mongodb.net/node-shop?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true})
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
