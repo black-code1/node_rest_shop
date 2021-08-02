50b2ddd4915880afc967cde3a6c954da2c1da762
black-code1 <legrandmahamat1@gmail.com>
2021-08-02 12:59:19 +0100

    Parsing the Body & Handling CORS Creating a REST API with Nodejs

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,14 +3,20 @@ const router = express.Router()
 
 // Handle incoming GET requests to /orders
 router.get('/',(req, res, next) => {
+ 
   res.status(200).json({
     message: 'Orders were fetched'
   })
 })
 
 router.post('/',(req, res, next) => {
+  const order = {
+    productId: req.body.productId,
+    quantity: req.body.quantity
+  }
   res.status(201).json({
-    message: 'Order was created'
+    message: 'Order was created',
+    order: order
   })
 })
 
