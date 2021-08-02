3483ebc3a6f7afa2ad78fca19d230e4520187475
black-code1 <legrandmahamat1@gmail.com>
2021-08-02 23:30:25 +0100

    Populating Queries with Mongoose

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -10,6 +10,7 @@ router.get('/',(req, res, next) => {
   Order
       .find()
       .select('product quantity _id')
+      .populate('product', 'name')
       .exec()
       .then(docs => {
         res.status(200).json({
@@ -80,6 +81,7 @@ router.post('/',(req, res, next) => {
 router.get('/:orderId',(req, res, next) => {
   Order
       .findById(req.params.orderId)
+      .populate('product')
       .exec()
       .then(order => {
         if(!order){
